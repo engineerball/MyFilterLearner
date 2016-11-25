70b67468226092e80a955d9b9a2c49d85b1db95e
Teerapat Khunpech <engineerball@gmail.com>
2016-11-26 00:42:39 +0700

    add prediction

--- MyFilteredLearner.java
+++ MyFilteredLearner.java
@@ -101,11 +101,62 @@ public class MyFilteredLearner {
 			classifier = new FilteredClassifier();
 			classifier.setFilter(filter);
 			classifier.setClassifier(new RandomForest());
-      classifier.buildClassifier(trainData);
+			classifier.buildClassifier(trainData);
 			// System.out.println(testData);
+			// String options[] = {"-v"};
 			Evaluation eval = new Evaluation(trainData);
 			// eval.crossValidateModel(classifier, trainData, 4, new Random(1));
-      eval.evaluateModel(classifier, testData);
+			eval.evaluateModel(classifier, testData);
+			
+			int numTestInstances = testData.numInstances();
+			System.out.printf("There are %d test instances\n", numTestInstances);
+			// Loop over each test instance.
+		    for (int i = 0; i < numTestInstances; i++)
+		    {
+		        // Get the true class label from the instance's own classIndex.
+		        String trueClassLabel = 
+		            testData.instance(i).toString(testData.classIndex());
+
+		        // Make the prediction here.
+		        double predictionIndex = 
+		            classifier.classifyInstance(testData.instance(i)); 
+
+		        // Get the predicted class label from the predictionIndex.
+		        String predictedClassLabel =
+		        		testData.classAttribute().value((int) predictionIndex);
+
+		        // Get the prediction probability distribution.
+		        double[] predictionDistribution = 
+		            classifier.distributionForInstance(testData.instance(i)); 
+
+		        // Print out the true label, predicted label, and the distribution.
+		        System.out.printf("%5d: true=%-10s, predicted=%-10s, distribution=", 
+		                          i, trueClassLabel, predictedClassLabel); 
+
+		        // Loop over all the prediction labels in the distribution.
+		        for (int predictionDistributionIndex = 0; 
+		             predictionDistributionIndex < predictionDistribution.length; 
+		             predictionDistributionIndex++)
+		        {
+		            // Get this distribution index's class label.
+		            String predictionDistributionIndexAsClassLabel = 
+		            		testData.classAttribute().value(
+		                    predictionDistributionIndex);
+
+		            // Get the probability.
+		            double predictionProbability = 
+		                predictionDistribution[predictionDistributionIndex];
+
+		            System.out.printf("[%10s : %6.3f]", 
+		                              predictionDistributionIndexAsClassLabel, 
+		                              predictionProbability );
+		        }
+
+		        System.out.println("\n");
+		    }
+			
+			
+			
 			System.out.println(eval.toSummaryString("\nResults\n======\n", false));
 			System.out.println(eval.toClassDetailsString());
 			System.out.println("===== Evaluating on filtered (training) dataset done =====");
